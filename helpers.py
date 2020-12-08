1436b6cf9016b83e65f6b09b03ab1f9fb997dbf8
Ricky Zhang <haoyuzhangca@outlook.com>
2020-12-07 23:19:46 -0500

    clean up

--- helpers.py
+++ helpers.py
@@ -19,10 +19,27 @@ def save(path, name, lis, mode):
 '''
 read a csv/txt file to list of int/float
 '''
-def read(path, name, mode):
+def read(path, name, rounding, mode):
     lis = []
     if mode == '.txt':
         with open(path + name + mode) as f:
             for line in f:
-                lis.append(round(float(line.strip('\n'))))
+                if rounding:
+                    lis.append(round(float(line.strip('\n'))))
+                else:
+                    lis.append(float(line.strip('\n')))
     return lis
+
+
+'''
+split a list into mean list
+'''
+def means(lis, length):
+    means = []
+    mean = 0
+    for i in range(len(lis)):
+        mean += lis[i]
+        if i % length == 0 and i != 0:
+            means.append(mean // 10)
+            mean = 0
+    return means
--- plot.py
+++ plot.py
@@ -5,55 +5,15 @@ import numpy as np
 import helpers
 
 '''
-gs = gridspec.GridSpec(2,2)
-
-# fig, (ax1, ax2) = plt.subplots(1, 2)
-fig = plt.figure()
-fig.suptitle("Train")
-ax1 = fig.add_subplot(gs[0,0])
-scores = []
-with open('./trained/scores.txt') as f:
-    for line in f:
-        scores.append(round(float(line.strip('\n'))))
-
-ax1.set_title("recorded score")
-ax1.set_xlabel("episode")
-ax1.plot(scores)
-# plt.show()
-
-ax2 = fig.add_subplot(gs[0,1])
-scores = np.array_split(scores, 500)
-temp = []
-for score in scores:
-    temp.append(np.mean(score))
-ax2.set_title('mean socre')
-ax2.set_xlabel("episode")
-ax2.plot(temp)
-# plt.show()
-
-ax3 = fig.add_subplot(gs[1,:])
-loss = []
-with open('./trained/losses.txt') as f:
-    for line in f:
-        loss.append(float(line.strip('\n')))
-ax3.set_title("loss")
-ax3.set_xlabel("episode")
-ax3.plot(loss)
-
-plt.show()
-'''
-
-'''
-plot scores and losses captured during the training
-'''
-
-
-'''
 plot the game scores from playing 2048 game with random policy vs trained policy
 '''
 
-plt.plot(helpers.read(path='./played/', name='final_value_random', mode='.txt'))
-plt.plot()
+plt.plot(helpers.read(path='./trained/', name='losses', rounding=False, mode='.txt'))
+plt.show()
 
-plt.plot(helpers.read(path='./played/', name='final_value_trained', mode='.txt'))
+plt.plot(helpers.means(helpers.read(path='./trained/', name='scores', rounding=False, mode='.txt'),100))
+plt.show()
+
+plt.plot(helpers.means(helpers.read(path='./played/', name='final_value_random', rounding=False, mode='.txt'),5))
+plt.plot(helpers.means(helpers.read(path='./played/', name='final_value_trained', rounding=False, mode='.txt'),5))
 plt.show()
